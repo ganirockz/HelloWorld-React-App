bf5c6b40c5a0867e60f6a4592666a8a780ba7208
ganirockz <ganeshrapeti8@gmail.com>
2020-11-27 22:09:35 +0530

    [Ganesh]Added: Welcome to Hello world react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import logo from './assets/logo.jpg';
 import './App.css';
+import App2 from './App2';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  url = "http://bridgelabz.com/"
+  constructor(){
+    super()
+    this.state = {
+      userName:""
+    }
+  }
+
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url,"_blank");
+  }
+
+  onNameChange = (event) => {
+    console.log("Name is changed");
+    let name = even.target.value;
+    let nameRegex = RegExp("[A-Z]{1}[a-zA-Z]{2,}");
+    if(nameRegex.test(name)){
+    this.setState({userName: name});}
+    
+  }
+
+  render() {
+    return <div>
+      <div>
+      <h1>Hello {this.state.userName} from bridgelabz</h1>
+      <img src={logo} onClick={this.onClick} alt="hello"></img>
+      </div>
+      <div className="text-box">
+          <input onChange={this.onNameChange} />
+          <output-error id="name-error"></output-error>
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
