a66cbd7fea575323148468a9dcf68ad895532920
ganirockz <ganeshrapeti8@gmail.com>
2020-11-27 22:14:50 +0530

    [Ganesh]Added: display hello from bridgelabz in app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import logo from './assets/logo.jpg';
+import logo from './logo.svg';
 import './App.css';
-import App2 from './App2';
 import React from "react";
 
 class App extends React.Component{
-  url = "http://bridgelabz.com/"
-  constructor(){
-    super()
-    this.state = {
-      userName:""
-    }
-  }
-
-  onClick = ($event) => {
-    console.log("save button is clicked!", $event);
-    window.open(this.url,"_blank");
-  }
-
-  onNameChange = (event) => {
-    console.log("Name is changed");
-    let name = even.target.value;
-    let nameRegex = RegExp("[A-Z]{1}[a-zA-Z]{2,}");
-    if(nameRegex.test(name)){
-    this.setState({userName: name});}
-    
-  }
-
   render() {
     return <div>
-      <div>
-      <h1>Hello {this.state.userName} from bridgelabz</h1>
-      <img src={logo} onClick={this.onClick} alt="hello"></img>
-      </div>
-      <div className="text-box">
-          <input onChange={this.onNameChange} />
-          <output-error id="name-error"></output-error>
-      </div>
+        <h1>Hello from BridgeLabz</h1>
     </div>
   }
 }
