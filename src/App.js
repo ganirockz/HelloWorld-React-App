58e62bce0c6c0f1b7a6bc9418b40cdb31851e847
ganirockz <ganeshrapeti8@gmail.com>
2020-11-27 22:43:01 +0530

    [Ganesh]Added: launch a site on clicking the logo

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import React from "react";
 
 class App extends React.Component{
+  url = "http://bridgelabz.com"
   constructor(){
     super();
     this.state = {
       title: "hello form bridgelabz"
     }
   }
+
+  onClick = ($event)=>{
+    console.log("save button is clicked:");
+    window.open(this.url,"_blank");
+  }
+
   render() {
     return <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="The BridgeLabz logo: a Bridge to Employment through lab works"></img>
+        <img onClick={this.onClick} src={logo} alt="The BridgeLabz logo: a Bridge to Employment through lab works"></img>
     </div>
   }
 }
