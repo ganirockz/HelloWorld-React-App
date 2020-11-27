8b0ad397e7d6d4f9602ac36443d231b0be525888
ganirockz <ganeshrapeti8@gmail.com>
2020-11-27 23:15:23 +0530

    [Ganesh]Added: display hello from bridgelabz with user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      title: "hello form bridgelabz"
+      userName: ""
     }
   }
 
@@ -15,11 +15,21 @@ class App extends React.Component{
     console.log("save button is clicked:");
     window.open(this.url,"_blank");
   }
+  
+  onNameChange = (event)=>{
+    console.log("value is ", event.target.value);
+    this.setState({ userName: event.target.value})
+  }
 
   render() {
-    return <div>
-        <h1>{this.state.title}</h1>
+    return <div> 
+      <div>
+        <h1> Hello {this.state.userName} from BridgeLabz</h1>
         <img onClick={this.onClick} src={logo} alt="The BridgeLabz logo: a Bridge to Employment through lab works"></img>
+      </div>
+      <div>
+        <input onChange={this.onNameChange} />
+      </div>
     </div>
   }
 }
