ac44bf627da2cee714ce9812e32237640e71ebf9
ganirockz <ganeshrapeti8@gmail.com>
2020-12-16 18:26:03 +0530

    [Ganesh]Added: complete hello from bridgelabz with complete text

--- src/App.js
+++ src/App.js
@@ -29,14 +29,27 @@ class App extends React.Component{
   }
 
   render() {
-    return <div> 
-      <div>
+    return <div className="app-main"> 
+      <div className="main">
         <h1> Hello {this.state.userName} from BridgeLabz</h1>
         <img onClick={this.onClick} src={logo} alt="The BridgeLabz logo: a Bridge to Employment through lab works"></img>
-      </div>
-      <div>
+      <div className="text-box">
         <input onChange={this.onNameChange} />
         <span className="error-output">{this.state.nameError}</span>
+      </div>
+      <div>
+        At BridgeLabz we're a community of
+      <ul>
+        <div>technologists</div>
+        <div>thinkers</div>
+        <div>builders</div>
+      </ul>
+      Working together to keep the Tech Employability of Employeers alive....
+      <br />
+        <br />
+      To Know about us.visit <a href="http://bridgelabz.com/" rel="noreferrer" target="_blank">BridgeLabz</a> to learn more about our mission
+      i.e <b>Employability to all</b>
+      </div>
       </div>
     </div>
   }
